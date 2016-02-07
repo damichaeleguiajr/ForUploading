c3427f4a1b59b3a461b5766449c9f9e63cf13ead
Dan <danmichaeleguiajr@gmail.com>
2016-02-07 16:14:03 +0800

    Connection

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
 	$hostname = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "ehkasi_db";
+	$username = "u671731253_ekb";
+	$password = "081093daj";
+	$dbname = "u671731253_ekb";
 	$error = "Cannot connect to the database..";
 
 	$sqlCon = mysql_connect($hostname,$username,$password) or die ($error);
